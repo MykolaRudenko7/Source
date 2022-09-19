abc2461e98eb3830f1a233af8a0623405b51f113
Mykola Rudenko <m.rudenko.rb@gmail.com>
2022-09-19 13:05:59 +0300

    tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,18 @@
 import "./slider";
 import modals from "./modules/modals";
+import tabs from "./modules/tabs";
 
 window.addEventListener("DOMContentLoaded", () => {
   modals();
+
+  //   таби 1
+  tabs(".glazing_slider", ".glazing_block", ".glazing_content", "active");
+
+  //   таби 2
+  tabs(
+    ".decoration_slider",
+    ".no_click",
+    ".decoration_content > div > div",
+    "after_click"
+  );
 });
---
+++ src/js/modules/tabs.js
@@ -0,0 +1,59 @@
+const tabs = (headerSel, tabSel, contentSel, activeClass) => {
+  const header = document.querySelector(headerSel);
+  const tabs = document.querySelectorAll(tabSel);
+  const content = document.querySelectorAll(contentSel);
+  const active = document.querySelector(activeClass);
+
+  // функція для ховання контенту табу (для кожного елементу)
+  function hideTabContent() {
+    // кожен елемент  контенту ховаю
+    content.forEach((element) => {
+      element.style.display = "none";
+    });
+
+    // таби теж, відбираючи  у них активний клас
+    tabs.forEach((tab) => {
+      tab.classList.remove(activeClass);
+    });
+  }
+
+  // ф-ція для показу контенту конкретного табу (за- замовчуванням 1й)
+  function showTabContent(i = 0) {
+    // показую конкретний ел контенту
+    content[i].style.display = "block";
+    //  і таб
+    tabs[i].classList.add(activeClass);
+  }
+
+  // щоб правильно показать таби, спочатку ховаю їх
+  hideTabContent();
+  // і потім показую
+  showTabContent();
+
+  // при кліку на блок з табами
+  header.addEventListener("click", function (e) {
+    const target = e.target;
+
+    //  якщо клік був на один з табів
+    //  (рег вираз вирізає точку з класу (для classList))
+    if (
+      target &&
+      (target.classList.contains(tabSel.replace(/\./, "")) ||
+        //  можна з closest()
+        target.parentNode.classList.contains(tabSel.replace(/\./, "")))
+    ) {
+      // то перебираю таби
+      tabs.forEach((tab, i) => {
+        // якщо клік на таб чи його елемент, берем його номер
+        if (target == tab || target.parentNode == tab) {
+          // закриваю всі
+          hideTabContent();
+          // показую який треба (на який клік)
+          showTabContent(i);
+        }
+      });
+    }
+  });
+};
+
+export default tabs;
